10ecda3f222bdef6eecaa3f3c167ef0a1af77b6f
melecouvreur <112541602+melecouvreur@users.noreply.github.com>
2023-03-25 08:05:41 +0000

    copied css from example + comments

--- my-react-app/src/Hooks/useCountDown.js
+++ my-react-app/src/Hooks/useCountDown.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+//function which performs countdown calculation
+//passes targetdate from breakPage
 const useCountdown = (targetDate) => {
   const countDownDate = new Date(targetDate).getTime();
 
@@ -7,6 +9,7 @@ const useCountdown = (targetDate) => {
     countDownDate - new Date().getTime()
   );
 
+  //setInterval browser API method to calculate the spare time every second(1000 milliseconds).
   useEffect(() => {
     const interval = setInterval(() => {
       setCountDown(countDownDate - new Date().getTime());

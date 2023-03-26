9cf3800663b5a0df83fbfa7b46d023a97e194b97
LouiseB96 <louisebond96@hotmail.com>
2023-03-26 21:12:36 +0100

    Added api fetch requests to services.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,32 +1,36 @@
-import React from "react"
+import React, { useEffect } from "react";
 import Home from "./Pages/Home";
 import DashBoard from "./Pages/DashBoard";
 import List from "./Pages/List";
 import BreakPage from "./Pages/BreakPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
+import Services from "./services";
 
 function App() {
-  return (
-    <div className="App">
-  
-        <p>
-          Hello ! Here are the Routes. 
-        </p>
+  const services = new Services();
+
+  //Added useEffect to test API calls on page load, this can be removed when we have components that can call it instead!
+
+  // useEffect(() => {
+  //   services.getFacts();
+  // }, []);
+
+  {
+    return (
+      <div className="App">
+        <p>Hello ! Here are the Routes.</p>
 
         <BrowserRouter>
-
-        <Routes>
-        
-        <Route path="/"element={<Home/>} />
-        <Route path="/dashboard"element={<DashBoard/>} />
-        <Route path="/list"element={<List/>} />
-        <Route path="/break"element={<BreakPage/>} />
-        </Routes>
-
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<DashBoard />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/break" element={<BreakPage />} />
+          </Routes>
         </BrowserRouter>
-
-    </div>
-  );
+      </div>
+    );
+  }
 }
 export default App;

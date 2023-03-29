07e22594857bf75ef21af3a7a410332d1ef5a3b2
Monica Lopez <monicalopeztamayo@gmail.com>
2023-03-29 17:56:43 +0100

    fix navbar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -39,7 +39,7 @@ function App() {
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ms-auto">
             <li class="nav-item">
               <Link to="/dashboard">
                 <a className="nav-link active">Home</a>

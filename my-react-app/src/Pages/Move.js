64162ea282161f15fd7e5aecec41672bfb9545f3
Monica Lopez <monicalopeztamayo@gmail.com>
2023-03-30 21:46:53 +0100

    start break

--- my-react-app/src/Pages/Move.js
+++ my-react-app/src/Pages/Move.js
@@ -1,13 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 function Move() {
   return (
     <div className="App">
-
       <p> I'm move </p>
-      
+      <Link to="/break">
+        <button className="button-outline">start</button>
+      </Link>
     </div>
-  )
+  );
 }
 export default Move;
--- my-react-app/src/Pages/Relax.js
+++ my-react-app/src/Pages/Relax.js
@@ -1,13 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 function Relax() {
   return (
     <div className="App">
-
+      <Link to="/break">
+        <button className="button-outline">start</button>
+      </Link>
       <p> I'm relax </p>
-      
     </div>
-  )
+  );
 }
 export default Relax;

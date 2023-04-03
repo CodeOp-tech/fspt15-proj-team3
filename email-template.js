2a8c949e33431b352f8c874d33c4c73aa49ceb21
melecouvreur <112541602+melecouvreur@users.noreply.github.com>
2023-04-03 14:08:43 +0100

    automated email and scheduling functionality created

---
+++ email-template.js
@@ -0,0 +1,57 @@
+const HTML_TEMPLATE = (text) => {
+    return `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>NodeMailer Email Template</title>
+          <style>
+            .container {
+              width: 100%;
+              height: 100%;
+              padding: 20px;
+              background-color: #f4f4f4;
+            }
+            .email {
+              width: 80%;
+              margin: 0 auto;
+              background-color: #fff;
+              padding: 20px;
+            }
+            .email-header {
+              background-color: #333;
+              color: #fff;
+              padding: 20px;
+              text-align: center;
+            }
+            .email-body {
+              padding: 20px;
+            }
+            .email-footer {
+              background-color: #333;
+              color: #fff;
+              padding: 20px;
+              text-align: center;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <div class="email">
+              <div class="email-header">
+                <h1>EMAIL HEADER</h1>
+              </div>
+              <div class="email-body">
+                <p>${text}</p>
+              </div>
+              <div class="email-footer">
+                <p>EMAIL FOOTER</p>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>
+    `;
+  }
+  
+  export default HTML_TEMPLATE;

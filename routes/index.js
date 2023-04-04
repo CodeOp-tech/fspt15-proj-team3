66620075fe60debcb30006d894ba2be8a612293f
melecouvreur <112541602+melecouvreur@users.noreply.github.com>
2023-04-04 08:47:01 +0100

    added stop5min route

--- routes/index.js
+++ routes/index.js
@@ -9,22 +9,28 @@ router.get('/', function(req, res, next) {
   res.send('index', { title: 'Express' });
 });
 
-//Reminders each Min - START - stopped working 
+//Reminders each Min - START  
 router.post('/reminders-start', async (req, res) => {
   await reminderEveryMin.start();
   return res.json({ message: 'Started' });
 });
 
-//Reminders each Min - STOP - stopped working
+//Reminders each Min - STOP 
 router.post('/reminders-stop', async (req, res) => {
   await reminderEveryMin.stop();
   return res.json({ message: 'Stopped' });
 });
 
-//Reminders each 5 Min - START - not working
+//Reminders each 5 Min - START
 router.post('/reminders-five-start', async (req, res) => {
   await reminderEveryFiveMin.start();
   return res.json({ message: 'Every 5 min reminder Started' });
+});
+
+//Reminders each 5 Min - STOP
+router.post('/reminders-five-stop', async (req, res) => {
+  await reminderEveryFiveMin.stop();
+  return res.json({ message: 'Every 5 min reminder Stopped' });
 });
 
 module.exports = router;
